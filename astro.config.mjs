28d87df260800b921406e1198a81bb19add1c957
dealparadize <ealejandrocasillas@gmail.com>
2024-12-31 16:10:21 -0700

    Add github action to deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,12 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
-import tailwind from '@astrojs/tailwind';
-import icon from 'astro-icon';
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+import tailwind from "@astrojs/tailwind";
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://example.com',
-    integrations: [mdx(), sitemap(), tailwind(), icon()],
+  site: "https://dealparadize.github.io",
+  integrations: [mdx(), sitemap(), tailwind(), icon()],
 });
